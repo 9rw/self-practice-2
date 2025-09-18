ae04a63620ae88931c618d9d3ea689d36a28c5e0
9rw <9rw9rw9@gmail.com>
2025-09-18 16:33:27 +0700

    complete lesson1 of week06

--- week06/starter/script.js
+++ week06/starter/script.js
@@ -10,6 +10,7 @@ const quotes = []
 */
 function addQuote(quote) {
   // TODO: Add the quote object to the quotes array
+  quotes.push(quote)
 }
 
 /*
@@ -19,6 +20,11 @@ function addQuote(quote) {
 */
 function deleteQuote(id) {
   // TODO: Remove the quote object from the array using the given id
+  quotes.forEach((obj, i)=> {
+    if (obj.id === id){
+      quotes.splice(i, 1)
+    }
+  })
 }
 
 /*
@@ -28,6 +34,11 @@ function deleteQuote(id) {
 */
 function updateQuote(id, updatedQuote) {
   // TODO: Find the quote by id and update its properties
+  quotes.forEach((obj, i)=> {
+    if (obj.id === id){
+      quotes.splice(i, 1, updatedQuote)
+    }
+  })
 }
 
 /*
@@ -36,13 +47,25 @@ function updateQuote(id, updatedQuote) {
 */
 function getAllQuotes() {
   // TODO: Return the quotes array
+  return quotes
 }
 
 // 6. Test your functions below
 // TODO: Add 3 quotes using addQuote()
+console.log("\nInitial Quotes:")
+addQuote({ id: 1, content: "The only limit to our realization of tomorrow is our doubts of today.", author: "Franklin D. Roosevelt" })
+addQuote({ id: 2, content: "In the middle of every difficulty lies opportunity.", author: "Albert Einstein" })
+addQuote({ id: 3, content: "It does not matter how slowly you go as long as you do not stop.", author: "Confucius" })
+console.log(getAllQuotes())
 
 // TODO: Delete 1 quote using deleteQuote()
+console.log("\nAfter Deleting Quote with id 2:")
+deleteQuote(2)
+console.log(getAllQuotes())
 
 // TODO: Update 1 quote using updateQuote()
+console.log("\nAfter Updating Quote with id 1:")
+updateQuote(1, { id: 1, content: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" })
 
 // TODO: Print all quotes using getAllQuotes()
+console.log(getAllQuotes())
