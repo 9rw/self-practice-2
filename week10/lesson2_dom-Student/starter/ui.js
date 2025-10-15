f88d1f4a848b6291585fe7b71aba4884b429059a
9rw <9rw9rw9@gmail.com>
2025-10-15 21:43:02 +0700

    update script

--- week10/lesson2_dom-Student/starter/ui.js
+++ week10/lesson2_dom-Student/starter/ui.js
@@ -1,8 +1,9 @@
+import { addQuote, getAllQuotes } from "./quote.js"
 // Step 1: Create an array to hold quote objects
-let quotes = undefined
+let quotes = []
 
 // Step 2: Select the DOM element where quotes will be rendered
-const quoteList = undefined
+const quoteList = document.querySelector("#quote-list");
 
 // Step 3: Define a function called renderQuotes()
 // This function should:
@@ -10,9 +11,23 @@ const quoteList = undefined
 // - Loop through the quotes array
 // - For each quote, create a <p> element with content and author
 // - Append each <p> to quoteList
-function renderQuotes() {}
+function renderQuotes() {
+  Array.from(quoteList.children).forEach((e)=> quoteList.removeChild(e))
+  
+  getAllQuotes().map(({id, author, content})=> {
+    const pElement = document.createElement("p")
+
+    pElement.textContent = `author: ${content} by ${author}`
+    quoteList.appendChild(pElement)
+  })
+  // console.log(c)
+  // console.log(quoteList.children)
+};
 
 // Step 4: Add test quotes manually and call renderQuotes()
 // Example:
 // addQuote('Stay hungry, stay foolish.', 'Steve Jobs')
 // renderQuotes();
+
+addQuote('Stay hungry, stay foolish.', 'Steve Jobs');
+renderQuotes();
